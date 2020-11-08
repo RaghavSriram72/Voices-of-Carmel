6fd650b283536ce983d6b4a491afef830dd08f71
Raghav Sriram <73449126+RaghavSriram72@users.noreply.github.com>
2020-11-07 22:55:33 -0500

    Add files via upload

---
+++ Contact-Form2.php
@@ -0,0 +1,26 @@
+<?php
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+ 
+$email_from = "$visitor_email.\n";
+ 
+$email_subject = "Feedback: $subject\n";
+ 
+$email_body = "Name: $firstname $lastname.\n".
+				"Message: $message\n";
+ 
+$to = "feedback@voicesofcarmel.com";
+ 
+ 
+$headers = "From: $email_from \r\n";
+ 
+$headers .= "Reply-To: $visitor_email \r\n";
+ 
+mail($to,$email_subject,$email_body,$headers);
+ 
+header("Location: index.html");
+ 
+?>
